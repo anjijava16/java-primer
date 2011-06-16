871b2d2479f49b73813b1e8789fae601b9e9f857
slieer <slieer@gmail.com>
2011-06-16 07:45:12 +0000

    modify java proxy.

--- simpleweb/JavaSE/com/proxy/simpleCat/Cat.java
+++ simpleweb/JavaSE/com/proxy/simpleCat/Cat.java
@@ -4,4 +4,10 @@ public class Cat implements IAnimal {
 	public void info() {
 		System.out.println("This is a cat!");
 	}
+
+	@Override
+	public int otherInfo() {
+		System.out.println("other info");
+		return 0;
+	}
 }
