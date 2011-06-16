58ab0a60bc81227917c575957cd95fc7b09cbeda
slieer@gmail.com <slieer@gmail.com@7319e136-6b89-d622-3f89-768691358c7b>
2011-06-16 07:08:24 +0000

    1、java basic: proxy , beanInfo 2、html5 canvas.

---
+++ trunk/simpleweb/JavaSE/com/beans/MyBean.java
@@ -0,0 +1,25 @@
+package com.beans;
+
+public class MyBean {
+	private String id = null;
+
+	private String name = null;
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+}
