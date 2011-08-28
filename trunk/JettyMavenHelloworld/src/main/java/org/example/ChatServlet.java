a87e534be3a9956827359827845e9806845e4adc
slieer@gmail.com <slieer@gmail.com@7319e136-6b89-d622-3f89-768691358c7b>
2011-08-28 15:35:54 +0000

    Tomcat doc, yahoo js evnet test.

--- trunk/JettyMavenHelloworld/src/main/java/org/example/ChatServlet.java
+++ trunk/JettyMavenHelloworld/src/main/java/org/example/ChatServlet.java
@@ -40,6 +40,10 @@ public class ChatServlet extends HttpServlet {
 			getServletContext().getNamedDispatcher("default").forward(request, response);
 	}	
 	
+	/**
+	 * 
+	 * @throws IOException
+	 */
 	private synchronized void join(HttpServletRequest request, HttpServletResponse response, String username) throws IOException {
 		Member member = new Member();
 		member._name = username;
@@ -73,15 +77,18 @@ public class ChatServlet extends HttpServlet {
 
 				buf.append("{\"action\":\"poll\",");
 				buf.append("\"from\":\"");
-				buf.append((String) member._queue.poll());
+				buf.append((String) member._queue.poll());   //每一个用户持有一个队列，队列里的元素成对出现（第一个用户名，第二个是消息。见chat方法）
 				buf.append("\",");
 
 				String message = (String) member._queue.poll();
+				
+				//把字符串的双引号"，加上转意符变成：\\"
 				int quote = message.indexOf('"');
 				while (quote >= 0) {
 					message = message.substring(0, quote) + '\\' + message.substring(quote);
 					quote = message.indexOf('"', quote + 2);
 				}
+				
 				buf.append("\"chat\":\"");
 				buf.append(message);
 				buf.append("\"}");
@@ -91,10 +98,12 @@ public class ChatServlet extends HttpServlet {
 			} else {
 				Continuation continuation = ContinuationSupport.getContinuation(request);
 				if (continuation.isInitial()) {
+					//阴塞请求
 					continuation.setTimeout(20000L);
 					continuation.suspend();
 					member._continuation = continuation;
 				} else {
+					//让请求超时。
 					response.setContentType("text/json;charset=utf-8");
 					PrintWriter out = response.getWriter();
 					out.print("{action:\"poll\"}");
@@ -102,7 +111,10 @@ public class ChatServlet extends HttpServlet {
 			}
 		}
 	}
-
+	
+	/**
+	 *用户有聊天记录时，把聊天记录加到用户自己的队列，如果用户的在server上有阴塞的标记，且 ？？？
+	 */
 	private synchronized void chat(HttpServletRequest request, HttpServletResponse response, String username, String message) throws IOException {
 		Map<String, Member> room = (Map<String, Member>) this._rooms.get(request.getPathInfo());
 		if (room != null) {
