fa8a4a38319b3f83bd26702e1dd1b0c076fcaea0
slieer <slieer@gmail.com>
2011-12-03 09:44:10 +0000

    para 2,3 code test.

---
+++ JavaSoaCookbook/src/java/com/sc/ch02/jaxb/Author.java
@@ -0,0 +1,22 @@
+package com.sc.ch02.jaxb;
+
+public class Author {
+	private String firstName;
+	private String lastName;
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+}
