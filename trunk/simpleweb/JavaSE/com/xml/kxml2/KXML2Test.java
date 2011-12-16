05af5c3a5989360b2489a1420e2f3bddc53cc9c0
slieer@gmail.com <slieer@gmail.com@7319e136-6b89-d622-3f89-768691358c7b>
2011-12-16 10:50:51 +0000

    xml test.

---
+++ trunk/simpleweb/JavaSE/com/xml/kxml2/KXML2Test.java
@@ -0,0 +1,38 @@
+package com.xml.kxml2;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.net.URL;
+
+import org.junit.Test;
+import org.kxml2.io.KXmlParser;
+import org.xmlpull.v1.XmlPullParser;
+
+public class KXML2Test {
+	@Test
+	public void t() throws Exception {
+		URL url = this.getClass().getResource("/");
+		String xmlFile = url.getPath() + "com/xml/unformattedXML.xml";
+		File file = new File(xmlFile);
+
+		InputStream stream = new FileInputStream(file);
+		KXmlParser parser = new KXmlParser();
+		parser.setInput(stream, null);
+		int eventType = parser.getEventType();
+		
+		while (eventType != XmlPullParser.END_DOCUMENT) {
+			switch (eventType) {
+			case XmlPullParser.START_DOCUMENT:
+				
+				break;
+			case XmlPullParser.END_DOCUMENT:
+				
+				break;
+			case XmlPullParser.START_TAG:
+				
+				break;
+			}
+		}
+	}
+}
