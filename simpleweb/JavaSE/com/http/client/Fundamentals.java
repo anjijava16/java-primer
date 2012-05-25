7629738bbef7ea0e103ecd22c943c9717875f4b6
slieer <slieer@gmail.com>
2012-05-25 11:15:05 +0000

    js test.

--- simpleweb/JavaSE/com/http/client/Fundamentals.java
+++ simpleweb/JavaSE/com/http/client/Fundamentals.java
@@ -1,20 +1,41 @@
 package com.http.client;
 
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.UnsupportedEncodingException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
 import java.text.MessageFormat;
 import java.util.Scanner;
 
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
+import org.apache.http.HttpVersion;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPut;
+import org.apache.http.conn.ClientConnectionManager;
+import org.apache.http.conn.params.ConnManagerParams;
+import org.apache.http.conn.params.ConnPerRoute;
+import org.apache.http.conn.params.ConnPerRouteBean;
+import org.apache.http.conn.scheme.PlainSocketFactory;
+import org.apache.http.conn.scheme.Scheme;
+import org.apache.http.conn.scheme.SchemeRegistry;
 import org.apache.http.entity.InputStreamEntity;
 import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.impl.conn.tsccm.ThreadSafeClientConnManager;
+import org.apache.http.params.BasicHttpParams;
 import org.apache.http.params.HttpConnectionParams;
-
-import sun.util.logging.resources.logging;
+import org.apache.http.params.HttpParams;
+import org.apache.http.params.HttpProtocolParams;
+import org.apache.http.util.EntityUtils;
+import org.junit.Test;
 
 
 public class Fundamentals {
@@ -101,4 +122,70 @@ public class Fundamentals {
         	e.printStackTrace();
         } 
 	}
-}
+	
+	@Test
+	public void getBasic(){
+		String urlStr = "http://www.wllxy.net/article.aspx?id=107";
+		try {
+			URL url = new URL(urlStr);
+			HttpURLConnection httpConn = (HttpURLConnection)url.openConnection();
+			httpConn.setRequestMethod("GET");			
+			httpConn.setDoInput(true);
+			InputStream in = httpConn.getInputStream();
+            BufferedReader br = new BufferedReader(new InputStreamReader(in,"gb2312"));
+
+            Scanner s = new Scanner(br);
+            while(s.hasNextLine()){
+            	String ori = s.nextLine();
+            	//String result = new String(ori.getBytes( "gb2312"));//不行
+            	System.out.println(ori);
+            }
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	
+	@Test
+	public void get(){		
+		String url = "http://www.wllxy.net/article.aspx?id=107";
+		//url = "http://www.baidu.com";
+		//url = "http://www.baidu.com";
+		
+		
+		Scheme scheme = new Scheme("http",80, PlainSocketFactory.getSocketFactory());
+		SchemeRegistry schemeRegistry = new SchemeRegistry();
+		schemeRegistry.register(scheme);
+		ClientConnectionManager cm = new ThreadSafeClientConnManager();
+		
+		HttpClient httpclient = new DefaultHttpClient(cm);
+        HttpGet get = new HttpGet(url);
+          
+        	
+        //get.setHeader("Accept-Language", "zh-cn,zh;q=0.8,en-us;q=0.5,en;q=0.3");
+        get.setHeader("User-Agent", "Mozilla/5.0(compatible;MSIE 9.0;Windows NT 6.1;Trident/5.0;BOIE9;ZHCN)");
+        get.setHeader("Accept","text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8");
+        //get.setHeader("Accept-Charset", "gb2312");
+        //get.setHeader("Accept-Charset", "utf-8");
+        //get.setHeader("Accept-Charset", "gbk");
+        //get.setHeader("Content-Type", "text/html;charset=gb2312");
+        //httppost.setHeader("Accept-Encoding","gzip,deflate");
+
+        try {
+            HttpResponse response = httpclient.execute(get);
+            HttpEntity resEntity = response.getEntity();
+            BufferedReader br = new BufferedReader(new InputStreamReader(resEntity.getContent(),"gb2312"));
+
+            Scanner s = new Scanner(br);
+            while(s.hasNextLine()){
+            	String ori = s.nextLine();
+            	//String result = new String(ori.getBytes( "gb2312"));//不行
+            	System.out.println(ori);
+            }
+        }catch(Exception e){
+        	e.printStackTrace();
+        }
+	}
+	
+}  
+
