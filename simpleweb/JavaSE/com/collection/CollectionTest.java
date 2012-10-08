121afe450d50a37bea11fbdb0bb83daa83d9cbe1
slieer <slieer@gmail.com>
2012-10-08 15:15:46 +0000

    java collection test.

--- simpleweb/JavaSE/com/collection/CollectionTest.java
+++ simpleweb/JavaSE/com/collection/CollectionTest.java
@@ -2,142 +2,105 @@ package com.collection;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
-import java.util.EnumMap;
-import java.util.EnumSet;
 import java.util.HashMap;
-import java.util.HashSet;
 import java.util.Iterator;
-import java.util.LinkedList;
 import java.util.List;
-import java.util.Map;
-import java.util.PriorityQueue;
-import java.util.Set;
-import java.util.TreeMap;
-import java.util.TreeSet;
 import java.util.Map.Entry;
+import java.util.Set;
 
-import org.apache.commons.logging.Log;
 import org.junit.Test;
 
-import simple.foundation.StringTest;
-
 public class CollectionTest {
-	static enum e{a,a1,a2};
 
-	void test(){
-		List list = new ArrayList();         //Object基本数组
-		List llist = new LinkedList();       //双项链表
+	void test() {
 
-		
-		Set set = new TreeSet();  //内部采用TreeMap 实现,红黑树。
-		set.toArray();
-		Set hset = new HashSet();  //内部采用HashMap实现。
-		//LinkedHashSet
-		
-		Set eSet =  EnumSet.noneOf(e.class);    //Enum[]
-		Set<e> set2 = EnumSet.allOf(e.class);
-		set2.toArray();
-		Set<e> set3 = EnumSet.of(e.a1, e.a2);
-
-		Map map = new HashMap();         // HashMap.Entry[]
-		//LinkedHashMap ;WeakHashMap;IdentityHashMap
-		
-		//TreeMap.Entry<K,V>  &  Node in the Tree(自已，孩子，父) & 红黑树（Red-Black tree）
-		Map tMap = new TreeMap();        
-	    Map eMap = new EnumMap(e.class); //Object基本数组
-	    
-	    PriorityQueue queue = new PriorityQueue(); //Object[] & balanced binary heap
 	}
-	
-	static void arrayToList(){
+
+	static void arrayToList() {
 		List<String> list = new ArrayList<String>();
 		String[] strs = new String[list.size()];
-		//Collections.
+		// Collections.
 		list.toArray(strs);
-		
+
 		arrayToList(strs);
 		arrayToList("");
 	}
-	
-	static void arrayToList(String... strs){
+
+	static void arrayToList(String... strs) {
 		System.out.println("multiple parameter.");
 		List<String> wordList = Arrays.asList(strs);
 	}
 
-	
-	static void arrayToList(String strs){
+	static void arrayToList(String strs) {
 		System.out.println("single parameter.");
 	}
-	
-	static Object[] arrayLen(){
-		return new Object[]{null, null};
+
+	static Object[] arrayLen() {
+		return new Object[] { null, null };
 	}
-	
-	
-	static void mapEntry(){
-		HashMap<Long, Boolean>purchasedGoodsIdsMap = new HashMap<Long, Boolean>();
+
+	static void mapEntry() {
+		HashMap<Long, Boolean> purchasedGoodsIdsMap = new HashMap<Long, Boolean>();
 		purchasedGoodsIdsMap.put(1L, false);
-		
-		Set<Entry<Long, Boolean>>  entrySet = purchasedGoodsIdsMap.entrySet();
-		
+
+		Set<Entry<Long, Boolean>> entrySet = purchasedGoodsIdsMap.entrySet();
+
 		Long[] ids = new Long[entrySet.size()];
 		int index = 0;
 		Iterator<Entry<Long, Boolean>> it = entrySet.iterator();
-		while(it.hasNext()){
-			Entry<Long,Boolean> entry = it.next();
-			if(! entry.getValue()){
+		while (it.hasNext()) {
+			Entry<Long, Boolean> entry = it.next();
+			if (!entry.getValue()) {
 				System.out.println("......");
 				ids[index++] = entry.getKey();
 			}
 		}
-		
+
 	}
-	
-	public void ff(Long... l){
+
+	public void ff(Long... l) {
 		System.out.println(l);
-		System.out.println(Arrays.asList(l));    //[1, 2, 3, 4]
+		System.out.println(Arrays.asList(l)); // [1, 2, 3, 4]
 	}
-	
-	public void f(long... l){
+
+	public void f(long... l) {
 		System.out.println(l);
 		System.out.println(Arrays.asList(l));
 	}
-	
+
 	@Test
-	public void testF(){
-		Long[] p = new Long[]{1L,2L,3L,4L};
-		long[] p1 = new long[]{1L,2L,3L,4L};
-		
+	public void testF() {
+		Long[] p = new Long[] { 1L, 2L, 3L, 4L };
+		long[] p1 = new long[] { 1L, 2L, 3L, 4L };
+
 		ff(p);
 		f(p1);
-		
+
 		f();
 		f(null);
-		
+
 		ff();
-		//ff(null);   //null Exception
+		// ff(null); //null Exception
 	}
-	
+
 	public static void main(String[] args) {
-		//arrayToList();
-		//System.out.println(arrayLen().length);
-		//mapEntry();
-		
+		// arrayToList();
+		// System.out.println(arrayLen().length);
+		// mapEntry();
+
 		List<Long> ids = new ArrayList<Long>();
 		ids.add(1L);
 		ids.add(11L);
 		ids.add(111L);
-		
+
 		Long[] idArr = new Long[ids.size()];
 		ids.toArray(idArr);
-		
+
 		System.out.println(idArr);
-		
-		if(idArr instanceof Long[]){
+
+		if (idArr instanceof Long[]) {
 			System.out.println("Long[]");
 		}
 	}
-	
-	
+
 }
---
+++ simpleweb/JavaSE/com/collection/list/ListTest.java
@@ -0,0 +1,14 @@
+package com.collection.list;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+public class ListTest {
+	List<String> list = new ArrayList<String>();         //Object基本数组
+	List<Ob> llist = new LinkedList<Ob>();       //双项链表
+	
+	static class Ob{
+		
+	}
+}
